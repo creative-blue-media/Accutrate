a75cb64b591f6b7f5516059afb736e33f30561c9
Carla <carlaheywood24@gmail.com>
2019-06-20 19:13:10 -0700

    Forgot Password page adjust

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -1,15 +1,8 @@
 <template>
   <b-row class="h-100">
-    <b-colxx xxs="12" md=10  class="mx-auto my-auto">
-      <b-card class="auth-card" no-body>
-          <div class="position-relative image-side ">
-            <p class=" text-white h2">{{ $t('dashboards.magic-is-in-the-details') }}</p>
-              <p class="white mb-0">  Please use this form to register. <br />If you are a member, please
-                <router-link tag="a" to="/user/login" class="white"><b>login</b></router-link>.
-              </p>
-          </div>
+    <b-colxx xxs="6" md=6  class="mx-auto my-auto">
+      <b-card class="auth-card" >
           <div class="form-side">
-            <router-link tag="a" to="/"><span class="logo-single"/></router-link>
             <h6 class="mb-4">{{ $t('user.register')}}</h6>
             <b-form @submit.prevent="formSubmit">
                <label class="form-group has-float-label mb-4">
