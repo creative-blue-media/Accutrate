f6a9de361452f8d76e58bccd4c48176b721e061a
Carla <carlaheywood24@gmail.com>
2019-07-07 21:55:38 -0700

    Color and Background Changes

--- src/views/website/Home.vue
+++ src/views/website/Home.vue
@@ -3,14 +3,40 @@
     <b-container class="home-header" fluid>
       <b-container class="h-header">
         <b-row class="home-pad" style="z-index: 999;">
-          <b-col xs=12 md=6>
+          <b-col class="header-text" xs=12 md=6>
             <h5>Better Breathing for Life</h5>
-            <h1 style="font-size: 4.5em;"><strong>Continuous SPO2 Monitoring Cloud Technology</strong></h1>
+            <h1 style="font-size: 4em;"><strong>Continuous SPO2 Monitoring Cloud Technology</strong></h1>
             <h5>Live Longer, Breathe Better</h5>
             <br>
             <b-button class="mb-1 button-home">{{ 'Learn More' }}</b-button>
           </b-col>
           <div class="box col-md-6">
+            <b-card sm="6" class="header-form mb-4">
+                <b-form class="mb-12">
+                  <b-row>
+                    <h2 style="padding:10px;">Get More Information</h2>
+                    <b-col sm="12">
+                      <b-form-group :label="$t('forms.firstname')"  >
+                        <b-form-input type="text" v-model="prospect.name" /> <!--:state="!$v.validateForm.firstname.$invalid"-->
+                        <b-form-invalid-feedback>{{ $t('forms.firstname-message')}}</b-form-invalid-feedback>
+                      </b-form-group>
+                    </b-col>
+                    <b-col sm="12">
+                      <b-form-group :label="$t('forms.lastname')">
+                        <b-form-input type="text" v-model="prospect.company"/>
+                        <b-form-invalid-feedback>{{ $t('forms.lastname-message')}}</b-form-invalid-feedback>
+                      </b-form-group>
+                    </b-col>
+                    <b-col sm="12">
+                      <b-form-group :label="$t('forms.email')">
+                        <b-form-input type="text" v-model="prospect.company"/>
+                        <b-form-invalid-feedback>{{ $t('forms.email-message')}}</b-form-invalid-feedback>
+                      </b-form-group>
+                    </b-col>
+                  </b-row>
+                  <b-button style="width:100%;" type="submit" variant="primary" class="mt-4">{{ $t('forms.submit') }}</b-button> <!--:disabled="$v.validateForm.$invalid"-->
+                </b-form>
+              </b-card>
           </div>
         </b-row>
       </b-container>
@@ -19,7 +45,7 @@
         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
           viewBox="0 0 2000 1117" style="enable-background:new 0 0 2000 1117;" xml:space="preserve">
         <title>SVGArtboard 3</title>
-        <path style="fill:#fff;" d="M0,1116.78c0,0,1360.42-586.57,2000-576.56l-0.42,576.78L0,1116.78z"/>
+        <path style="fill:#F5F9FD;" d="M0,1116.78c0,0,1360.42-586.57,2000-576.56l-0.42,576.78L0,1116.78z"/>
         </svg>
       </div>
     </b-container>
@@ -30,19 +56,21 @@
             <!-- <b-col md=12> -->
               <b-container fluid>
                 <b-row style="width: 80vw;">
-                  <b-col xs=12 md=12 style="height: 40vh; border-radius: 2%; background: linear-gradient(to bottom right, #77B2E6, #fff);">
+                  <b-col xs=12 md=6 style="height: 40vh; background: linear-gradient(to bottom right, #77B2E6, #304772);">
+                    <h1 style="margin: 0; color: #fff; border-bottom: 0px; text-align: center; padding: 15px;"><strong>The Problem</strong></h1>
                   </b-col>
-                  <b-col xs=12 md=12 style="height: 40vh; background: #fff;">
+                  <b-col xs=12 md=6 style="height: 40vh; background: #fff;">
                     <h1 style="color: #555; border-bottom: 0px; padding-top: 8px;">I QUO REPRIMIQUE </h1>
                     <hr>
                   </b-col>
                 </b-row>
                 <b-row style="width: 80vw;">
-                  <b-col md=12 style="height: 40vh; background: #fff;">
+                  <b-col md=6 style="height: 40vh; background: #fff;">
                     <h1 style="color: #555; border-bottom: 0px; padding-top: 8px;">I QUO REPRIMIQUE </h1>
                     <hr>
                   </b-col>
-                  <b-col md=12 style="height: 40vh; border-radius: 2%; background: linear-gradient(to bottom right, #77B2E6, #fff);">
+                  <b-col md=6 style="height: 40vh; background: linear-gradient(to bottom right, #77B2E6, #304772);">
+                    <h1 style="color: #fff; border-bottom: 0px; padding: 15px;"><strong>Our Solution</strong></h1>
                   </b-col>
                 </b-row>
               </b-container>
@@ -70,33 +98,33 @@
           </b-row>
       </b-container>
     </b-container>
-    <b-container fluid>
-      <b-container>
-        <b-row>
-          <h1>Contact</h1>
-        </b-row>
-        <b-row>
-          <b-col xxs="12">
-              <b-card class="mb-4" :title="$t('forms.validation')">
-                <b-form > 
+    <b-container class="contact-section" fluid>
+      <b-container class="contact">
+        <b-row style="height:auto; width:100%;">
+              <b-card class="footer-contact">
+                <b-form class="mr-0 ml-0" style="width:100%;">
                   <b-row>
-                    <b-col sm="6">
+                    <b-col sm="3">
+                      <h2>Subscribe to recieve the latest news</h2>
+                    </b-col>  
+                    <b-col sm="3">
                       <b-form-group :label="$t('forms.firstname')"  >
                         <b-form-input type="text" v-model="prospect.name" /> <!--:state="!$v.validateForm.firstname.$invalid"-->
                         <b-form-invalid-feedback>{{ $t('forms.firstname-message')}}</b-form-invalid-feedback>
                       </b-form-group>
                     </b-col>
-                    <b-col sm="6">
-                      <b-form-group :label="$t('forms.lastname')">
+                    <b-col sm="3">
+                      <b-form-group :label="$t('forms.email')">
                         <b-form-input type="text" v-model="prospect.company"/>
-                        <b-form-invalid-feedback>{{ $t('forms.lastname-message')}}</b-form-invalid-feedback>
+                        <b-form-invalid-feedback>{{ $t('forms.email-message')}}</b-form-invalid-feedback>
                       </b-form-group>
                     </b-col>
+                     <b-col sm="3">
+                      <b-button type="submit" variant="primary" class="mt-4" style="width:100%;">{{ $t('forms.submit') }}</b-button> <!--:disabled="$v.validateForm.$invalid"-->
+                     </b-col>
                   </b-row>
-                  <b-button type="submit" variant="primary" class="mt-4">{{ $t('forms.submit') }}</b-button> <!--:disabled="$v.validateForm.$invalid"-->
                 </b-form>
               </b-card>
-          </b-col>
         </b-row>
       </b-container>
     </b-container>
